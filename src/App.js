9c8e573fde947b1f0f3be08d3b63f6a8103075d1
Yulian Stefanov <yu_stefanov1@yahoo.bg>
2023-02-09 23:25:00 +0200

    added card visiablity state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 import Cart from "./components/Cart/Cart";
 
 function App() {
+ const [showCart, setShowCart] = useState(false);
+
+ const showCartHandler = () => {
+setShowCart(true)
+ };
+
+ const hideCartHandler = () => {
+setShowCart(false)
+ };
+
   return (
     <CartProvider>
       <Cart />
